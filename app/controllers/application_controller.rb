bc4f4868553986f4e579ba8b1bd6bba1b327ef73
Kaya Aykut <kaya.aykut@gmail.com>
2011-11-25 17:23:35 +0100

    added users. pending tests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
  # before_filter :go_to_index
+  before_filter :authorize, :except => [:login]
+  before_filter :set_currents
   
+
+  protected
+	def authorize
+    unless session[:user_id] and User.find(session[:user_id])
+      flash[:notice] = "Please log in"
+      redirect_to :controller => 'authorization', :action => 'login'
+    end
+  end
+
+	private
+	def user_session
+		@user_session ||= UserSession.new(session)
+	end
+
   def set_currents
     @current_controller = controller_name
     @current_action = action_name
   end
+  
   
   # def go_to_index
   #   redirect_to
---
+++ app/controllers/authorization_controller.rb
@@ -0,0 +1,52 @@
+#---
+# Excerpted from "Agile Web Development with Rails, 3rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails3 for more book information.
+#---
+class AuthorizationController < ApplicationController
+
+  # just display the form and wait for user to
+  # enter a name and password
+  
+  def login
+    
+    if request.post?
+      user = User.authenticate(params[:name], params[:password])
+      if user
+#KAYA 06/05/2011 - data_owner_id for now in session. might be security risk, as data_owner_id is CRITICAL. look into it.
+        session[:user_id] = user.id
+				session[:data_owner_id] = user.data_owner_id
+        redirect_to(:controller => "uploads", :action => "index")
+      else
+        flash[:error] = "Invalid user/password combination"
+      end
+    end
+  end
+  
+
+  
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to(:action => "login")
+  end
+  
+  def signup
+    
+    if request.post?
+    else
+      @user = User.new()
+      respond_to do |format|
+        format.html
+      end
+    end
+  end
+  
+  def index
+
+  end
+  
+end
